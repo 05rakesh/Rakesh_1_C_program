f29b0c8f9ae72eb9ea3706ff2d3df53ab81dfc09
Rakesh M Badiger <rmb01809@gmail.com>
2024-11-25 23:31:05 -0800

    Update string.c

--- string.c
+++ string.c
@@ -5,21 +5,21 @@ int main() {
     char str[100];
     int len, isPalindrome = 1;
 
-    // Input the string
+    
     printf("Enter a string: ");
     scanf("%s", str);
 
     len = strlen(str);
 
-    // Check for palindrome using a loop
+    
     for (int i = 0; i < len / 2; i++) {
         if (str[i] != str[len - i - 1]) {
-            isPalindrome = 0; // Not a palindrome
+            isPalindrome = 0; 
             break;
         }
     }
 
-    // Output the result
+    
     if (isPalindrome) {
         printf("The string \"%s\" is a palindrome.\n", str);
     } else {
