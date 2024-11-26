097b36ea84e4e29981a8821fbc4c650690ed601f
Rakesh M Badiger <rmb01809@gmail.com>
2024-11-26 00:45:49 -0800

    Update positive,negativeorzero.c

--- positive,negativeorzero.c
+++ positive,negativeorzero.c
@@ -3,11 +3,11 @@
 int main() {
     int num;
 
-    // Ask the user to input a number
+    
     printf("Enter a number: ");
     scanf("%d", &num);
 
-    // Using if statement to check if the number is positive, negative, or zero
+    
     if (num > 0) {
         printf("%d is positive.\n", num);
     } else if (num < 0) {
